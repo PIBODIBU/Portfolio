535e77c2c9a982ea4327615f45f4c6683180f4c5
Roman S <romeo97934@gmail.com>
2017-05-20 14:41:19 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var app = angular.module('PortfolioApp',
+    ['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngAnimate', 'ui.router']);
+
+app.config(function ($mdIconProvider, $mdThemingProvider, $stateProvider, $urlRouterProvider) {
+    $stateProvider.state('skills', {
+        url: '/skills',
+        templateUrl: 'template/skills.tmpl.html'
+    });
+
+    $stateProvider.state('projects', {
+        url: '/projects',
+        templateUrl: 'template/projects.tmpl.html'
+    });
+
+    $stateProvider.state('info', {
+        url: '/info',
+        templateUrl: 'template/info.tmpl.html'
+    });
+
+    $stateProvider.state('resizer', {
+        url: '/resizer',
+        templateUrl: 'template/page4.tmpl.html'
+    });
+
+    $urlRouterProvider.otherwise("/skills");
+
+    $mdIconProvider.defaultIconSet('/assets/icons/mdi.svg');
+
+    $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('amber')
+        .backgroundPalette('grey');
+
+    $mdThemingProvider.theme('dark')
+        .accentPalette('amber')
+        .backgroundPalette('teal')
+        .dark();
+
+    $mdThemingProvider.enableBrowserColor({
+        theme: 'default',
+        palette: 'primary',
+        hue: '800' // Default is '800'
+    });
+});
